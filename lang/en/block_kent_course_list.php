833c3cb1f9f6bb919916e2ee64e5389a332f5348
unknown <fg30@kent.ac.uk>
2012-01-25 16:29:33 +0000

    Addition of Moodle navigation features- first pass
    This adds the Navigational features to navigate between current and archive moodle in the Kent Course Overview block as well as adding the Kent Course List block, which is a block that overrides the functionality/output of the Course List block without modifying any core code.  See our Kent.Docs folder for details about adding bits to the config file to get this working.

---
+++ lang/en/block_kent_course_list.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'Kent course list';
+
+//Some settings specific to old Moodle to override the "All courses" text at the footer.
+$string['archive_moodle_old_text'] = 'All archive courses';
+$string['archive_moodle_current_text'] = 'All current courses';
