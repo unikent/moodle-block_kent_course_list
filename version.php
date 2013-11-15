a1fe73cfc6ab0045b2349d7a7a46321b880bcc5f
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-11-15 09:55:12 +0000

    Fix some installation errors

--- version.php
+++ version.php
@@ -28,6 +28,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2011112901;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->version   = 2012112901;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_kent_course_list'; // Full name of the plugin (used for diagnostics)
