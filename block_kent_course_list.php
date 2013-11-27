4416a492b2a3a06ce4605d4483e778a502559f76
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-11-27 09:47:04 +0000

    Deprecate kent course list

--- block_kent_course_list.php
+++ block_kent_course_list.php
@@ -4,6 +4,9 @@ include_once($CFG->dirroot . '/course/lib.php');
 include_once($CFG->dirroot . '/blocks/course_list/block_course_list.php');
 require_once($CFG->dirroot . '/blocks/kent_course_overview/lib.php'); // Leverage functions from kent course overview to get archive links
 
+/**
+ * Deprecated
+ */
 class block_kent_course_list extends block_course_list {
 
     function init() {
