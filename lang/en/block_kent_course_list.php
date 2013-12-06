3ea10136f1962068e54c40ac3d1321c609de2acd
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-12-06 15:16:26 +0000

    Add langs to course list

--- lang/en/block_kent_course_list.php
+++ lang/en/block_kent_course_list.php
@@ -1,3 +1,6 @@
 <?php
 $string['pluginname'] = 'Kent course list';
 $string['blocktitle'] = 'Modules';
+
+$string['kent_course_list:addinstance'] = 'Add a new Kent course list block';
+$string['kent_course_list:myaddinstance'] = 'Add a new Kent course list block to My home';
