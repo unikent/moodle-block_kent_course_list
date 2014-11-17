cc13a599553eac4f11b5ee39e928cc0bffd8b62e
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-17 10:46:21 +0000

    Remove KCO ref

--- block_kent_course_list.php
+++ block_kent_course_list.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once($CFG->dirroot . '/course/lib.php');
 include_once($CFG->dirroot . '/blocks/course_list/block_course_list.php');
-require_once($CFG->dirroot . '/blocks/kent_course_overview/lib.php'); // Leverage functions from kent course overview to get archive links
 
 /**
  * Deprecated
